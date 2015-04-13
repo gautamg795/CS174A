843e9af842c304a2e6ae515f7a414917f3bf1faf
Gautam Gupta <blerb795@gmail.com>
2015-04-12 20:59:55 -0700

    add comments

--- cubes.js
+++ cubes.js
@@ -1,24 +1,12 @@
 var canvas;
 var gl;
 
-var NumVertices = 14;
+var points = []; // The array of points to be loaded onto GPU for drawing
+var theta = 0; // For rotation of cubes
+var thetaLoc, modelViewProjectionLoc, vColorLoc; // For storing of uniform vars for shader
+var crosshairs = false; // Toggled to true when crosshairs should be shown
 
-var points = [];
-
-var xAxis = 0;
-var yAxis = 1;
-var zAxis = 2;
-
-var axis = 0;
-var theta = 0;
-
-var thetaLoc, modelViewProjectionLoc, vColorLoc;
-
-
-var projectionLoc;
-var crosshairs = false;
-
-var colorIndex = 0;
+var colorIndex = 0; // Used to put different colors on the cubes and cycle
 var colors = [
     [0.2, 0.2, 0.2, 1.0], // black
     [1.0, 0.0, 0.0, 1.0], // red
@@ -60,34 +48,37 @@ window.onload = function init() {
     generateCubes();
     generateCrosshair();
 
-    gl.viewport(0, 0, canvas.width, canvas.height);
-    camera.aspect = canvas.width / canvas.height;
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.viewport(0, 0, canvas.width, canvas.height); // Set viewport settings
+    camera.aspect = canvas.width / canvas.height; // Set aspect ratio
+    gl.clearColor(0.0, 0.0, 0.0, 1.0); // Initialize canvas to black
+    gl.enable(gl.DEPTH_TEST); // Enable Z buffer
 
 
-    //
+
     //  Load shaders and initialize attribute buffers
-    //
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
-
+    // Find where we store the color info
     vColorLoc = gl.getUniformLocation(program, "vColor");
 
+    // Load the points into the GPU
     var vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
 
+    // Make vPosition point to those points
     var vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
+    // Find where we store the rotation angle theta
     thetaLoc = gl.getUniformLocation(program, "theta");
+    // Find where we store the model-view-projection matrix
     modelViewProjectionLoc = gl.getUniformLocation(program, "modelViewProjection");
-    projectionLoc = gl.getUniformLocation(program, "projection");
 
+    // Use onkeypress for all non-arrow key events
     window.onkeypress = function (event) {
-        // If the key pressed represents an alphanumeric character, convert it; else let the key code pass through
         var key = String.fromCharCode(event.keyCode)
             .toLowerCase();
         switch (key) {
@@ -132,6 +123,7 @@ window.onload = function init() {
             break;
         }
     };
+    // Use onkeydown for arrow keys
     window.onkeydown = function (event) {
         switch (event.keyCode) {
         case 38: // Up arrow key
@@ -148,33 +140,35 @@ window.onload = function init() {
             break;
         }
     };
-    render();
 
+    render();
 }
 
 
 
 function generateCubes() {
+    // Note that we only create one set of vertices
     var vertices = [
-        vec3(-0.5, -0.5, 0.5),
-        vec3(-0.5, 0.5, 0.5),
-        vec3(0.5, 0.5, 0.5),
-        vec3(0.5, -0.5, 0.5),
-        vec3(-0.5, -0.5, -0.5),
-        vec3(-0.5, 0.5, -0.5),
-        vec3(0.5, 0.5, -0.5),
-        vec3(0.5, -0.5, -0.5)
+        vec3(-1.0, -1.0, 1.0),
+        vec3(-1.0, 1.0, 1.0),
+        vec3(1.0, 1.0, 1.0),
+        vec3(1.0, -1.0, 1.0),
+        vec3(-1.0, -1.0, -1.0),
+        vec3(-1.0, 1.0, -1.0),
+        vec3(1.0, 1.0, -1.0),
+        vec3(1.0, -1.0, -1.0)
     ];
 
+    // The traversal order for triangle strips
     var indices = [0, 4, 7, 6, 3, 2, 1, 6, 5, 4, 1, 0, 3, 7];
-
+    // Push the points for triangle strips
     for (var i = 0; i < indices.length; ++i) {
         points.push(vertices[indices[i]]);
-
-        // for solid colored faces use 
-        //colors.push(vertexColors[a]);
     }
-    var indices = [0, 1,
+
+    // Order in which to draw the lines separating the cube faces
+    indices = [
+        0, 1,
         1, 2,
         2, 3,
         3, 0,
@@ -186,7 +180,8 @@ function generateCubes() {
         1, 5,
         5, 6,
         4, 5
-    ]; // The index order for triangle strips
+    ];
+    // Push the points for line segments
     for (var i = 0; i < indices.length; ++i) {
         points.push(vertices[indices[i]]);
     }
@@ -205,12 +200,15 @@ function generateCrosshair() {
 }
 
 function render() {
+    // enable Z-buffer
     gl.enable(gl.DEPTH_TEST);
+    // clear the buffers
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    theta += 6.0; // 60 rpm?
-    gl.uniform1f(thetaLoc, theta);
-    var projectionMatrix = perspective(camera.fovy(), camera.aspect, camera.near, camera.far);
+    theta += 6.0; // 60 rpm given 60 fps
+    gl.uniform1f(thetaLoc, theta); // Send theta to GPU
+    var projectionMatrix = perspective(camera.fovy(), camera.aspect, camera.near, camera.far); // Generate proj matrix
+    // The list of instances we want and where
     var models = [
         translate(10, 10, 10),
         translate(10, 10, -10),
@@ -221,6 +219,7 @@ function render() {
         translate(-10, -10, 10),
         translate(-10, -10, -10)
     ];
+    // Generate the view matrix based off of the translation and rotation of the camera)
     var viewMatrix = mult(rotate(camera.heading, [0, 1, 0]), translate(camera.x, camera.y, camera.z));
     for (var i = 0; i < models.length; i++) {
         models[i] = mult(viewMatrix, models[i]); // Create the model-view matrix
@@ -231,13 +230,13 @@ function render() {
         gl.uniform4fv(vColorLoc, [1, 1, 1, 1]); // Send white for the outlines
         gl.drawArrays(gl.LINES, 14, 24); // Draw the outlines which are stored in the last 24 points in array
     }
+    // Generate crosshairs if enabled
     if (crosshairs) {
-        gl.disable(gl.DEPTH_TEST);
-        gl.uniform1f(thetaLoc, 0);
-        projectionMatrix = ortho(camera.aspect * -3, camera.aspect * 3, 3, -3, 0, 1);
-        gl.uniformMatrix4fv(modelViewProjectionLoc, false, flatten(projectionMatrix));
-        gl.drawArrays(gl.LINES, 38, 4);
+        gl.disable(gl.DEPTH_TEST); // Disable depth buffer so nothing comes in front of crosshair (it shouldn't, though)
+        gl.uniform1f(thetaLoc, 0); // Send 0 for theta (crosshair shouldn't be rotated or scaled)
+        projectionMatrix = ortho(camera.aspect * -3, camera.aspect * 3, 3, -3, 0, 1); // Use orthographic projection
+        gl.uniformMatrix4fv(modelViewProjectionLoc, false, flatten(projectionMatrix)); // Send the projection matrix
+        gl.drawArrays(gl.LINES, 38, 4); // Draw the crosshairs
     }
     requestAnimFrame(render);
-
 }
