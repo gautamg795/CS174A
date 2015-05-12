3b45df560a6c2fc7b308e9c70dd220df3c610fce
Gautam Gupta <blerb795@gmail.com>
2015-05-12 04:13:40 -0700

    minor changes

--- textures.js
+++ textures.js
@@ -45,7 +45,7 @@ function configureTexture(image) {
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB,
         gl.RGB, gl.UNSIGNED_BYTE, image);
-    gl.generateMipmap(gl.TEXTURE_2D);
+    // gl.generateMipmap(gl.TEXTURE_2D);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
         gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
@@ -210,7 +210,7 @@ var render = function() {
         translate(-1.5, -.25, 0)
     ];
     if (scrolling) {
-        offset[1] += .005;
+        offset[1] += .01;
         if (offset[1] > 1) offset[1] -= 1.0;
     }
     for (var i = 0; i < models.length; i++) {
